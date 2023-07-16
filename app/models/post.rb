364ee4de4ac283c71ea922479b70f1a31fe5b27e
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-07-16 17:16:28 +0000

    [update]model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  validates :comment,presence:true
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :posts
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  
+  has_one_attached :profile_image
+  
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
 end
