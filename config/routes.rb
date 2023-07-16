32da22554373e62a04d15c0f8ac38360622eddf4
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-07-16 01:59:32 +0000

    [add]controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+# 顧客用
+devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  
   root to: "homes#top"
 end
