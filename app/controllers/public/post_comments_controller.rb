397e8152747b30a1a9f415a792620d3a8f2e6f67
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-07-29 04:34:52 +0000

    [update]post_comment

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,13 +2,14 @@ class Public::PostCommentsController < ApplicationController
   def create
     @review = Review.find(params[:review_id])
     @comment = current_user.post_comments.new(post_comment_params)
-    @comment.review_id = review.id
+    @comment.review_id = @review.id
     @comment.save
+    redirect_to review_path(@review)
   end
 
   def destroy
-    @comment = PostComment.find(params[:id])
-    @comment.destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to review_path(params[:review_id])
   end
 
   private
