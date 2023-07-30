395e4b098afafd8671a525fb1dc79904e3536942
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-07-30 02:22:39 +0000

    [fix] routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,8 @@ Rails.application.routes.draw do
   }
   
   devise_scope :user do
-    post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    get 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
   end
 
   scope module: :public do
