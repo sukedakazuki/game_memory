0e544af2b29e92f09cd4d8c9c2fd63c361b4f0e5
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-07-30 05:19:19 +0000

    [fix] bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,7 @@ Rails.application.routes.draw do
   }
   
   devise_scope :user do
-    get 'users/sign_out', to: 'public/sessions#destroy'
-    get 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
+    post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
   end
 
   scope module: :public do
