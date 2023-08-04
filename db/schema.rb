7716df58d03259fbbdb38315ed11143b74eb7e5f
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-08-04 14:33:20 +0000

    [fix] migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_03_142248) do
+ActiveRecord::Schema.define(version: 2023_08_03_142340) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
