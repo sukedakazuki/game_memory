3ba6ae47ceeabbac8300e7c735d5d6ed036c6ac4
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-08-07 17:26:07 +0000

    [update] post_comment

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,4 +1,6 @@
 class Public::PostCommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   def create
     @review = Review.find(params[:review_id])
     @comment = current_user.post_comments.new(post_comment_params)
@@ -15,5 +17,12 @@ class Public::PostCommentsController < ApplicationController
   private
   def post_comment_params
     params.require(:post_comment).permit(:comment)
+  end
+  
+  def ensure_correct_user
+    @review = Review.find(params[:id])
+    unless @review.user == current_user
+      redirect_to reviews_path
+    end
   end
 end
