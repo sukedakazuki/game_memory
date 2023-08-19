a86f76965ea1a9135eab9c135c462c7d1f492e12
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-08-19 13:26:31 +0000

    [update] rank

--- app/controllers/public/ranks_controller.rb
+++ app/controllers/public/ranks_controller.rb
@@ -3,5 +3,6 @@ class Public::RanksController < ApplicationController
   def rank
     # 投稿のいいね数ランキング
     @favorite_ranks = Review.find(Favorite.group(:review_id).order('count(review_id) desc').pluck(:review_id))
+    @reviews = Review.page(params[:page])
   end
 end
