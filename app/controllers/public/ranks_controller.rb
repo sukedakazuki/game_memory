34c4f094836ad6ebc00f21340dd85aeb78d205b8
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-08-23 20:05:00 +0000

    [update] header

--- app/controllers/public/ranks_controller.rb
+++ app/controllers/public/ranks_controller.rb
@@ -1,7 +1,9 @@
 class Public::RanksController < ApplicationController
   before_action :authenticate_user!
   def rank
-    #  投稿のいいね数週間ランキング
+    # 投稿のいいね数週間ランキング
     @favorite_ranks = Review.find(Favorite.group(:review_id).where(created_at: Time.current.all_month).limit(10).order('count(review_id) desc').pluck(:review_id))
+    # 投稿のコメント数週間ランキング
+    @post_comment_ranks = Review.find(PostComment.group(:review_id).limit(10).order('count(review_id) desc').pluck(:review_id))
   end
 end
