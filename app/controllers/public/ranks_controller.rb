f984d8411b21512366f1333b63b352f5f21bf451
sukedakazuki <130646268+sukedakazuki@users.noreply.github.com>
2023-09-05 14:19:16 +0000

    [update] rank

--- app/controllers/public/ranks_controller.rb
+++ app/controllers/public/ranks_controller.rb
@@ -8,8 +8,10 @@ class Public::RanksController < ApplicationController
   end
   
   def comment_rank
+    par_page = 10
+    @start = ((params[:page] || 1 ).to_i - 1) * par_page + 1
     # 投稿のコメント数週間ランキング
-    @comment_ranks = Review.find(PostComment.group(:review_id).limit(10).order('count(review_id) desc').pluck(:review_id))
+    @comment_ranks = Kaminari.paginate_array(Review.find(PostComment.group(:review_id).order('count(review_id) desc').pluck(:review_id))).page(params[:page])
   end
   
   def game_rank
